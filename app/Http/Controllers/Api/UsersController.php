fdd0a4b8a1cb80df49198fa768bc8333dcf57eb3
Alleo Indong <freedom_war550@live.com.ph>
2017-06-19 13:36:55 +0800

    added passport auth

---
+++ app/Http/Controllers/Api/UsersController.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Http\Request;
+
+class UsersController extends Controller {
+    public function me(Request $request)
+    {
+        $user = $request->user();
+
+        if (! $user) {
+            return response()->json(['unauthenticated'], 401);
+        }
+
+        $user = User::with('profile')
+            ->where('id', $user->id)
+            ->first();
+
+        return response()->json($user, 200);
+    }
+}
