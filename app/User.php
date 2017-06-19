ae2f401cb1d4f56cfeec72c7680d203e3507c29f
Alleo Indong <freedom_war550@live.com.ph>
2017-06-19 13:21:47 +0800

    added oauth2

--- app/User.php
+++ app/User.php
@@ -7,10 +7,11 @@ use App\Models\UserProfile;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use Notifiable, SoftDeletes;
+    use Notifiable, SoftDeletes, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
