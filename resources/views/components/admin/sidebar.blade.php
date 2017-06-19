6af0fee5c41def1acb0e1ed4f75e6b34509cad3c
Nina Kristel Cheng <nkmcheng@live.com>
2017-06-19 14:52:48 +0800

    update

--- resources/views/components/admin/sidebar.blade.php
+++ resources/views/components/admin/sidebar.blade.php
@@ -3,16 +3,6 @@
     <!-- sidebar: style can be found in sidebar.less -->
     <section class="sidebar">
 
-        <!-- Sidebar user panel (optional) -->
-        <div class="user-panel">
-            <div class="pull-left image">
-                <img src="/css/adminlte/img/user2-160x160.jpg" class="img-circle" alt="User Image">
-            </div>
-            <div class="pull-left info">
-                <p>{{ Auth::user()->profile->full_name }}</p>
-            </div>
-        </div>
-
         <!-- Sidebar Menu -->
         <ul class="sidebar-menu">
             <li class="header">MENU</li>
