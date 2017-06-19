2d6f922796b2d2995b4069cd591bdfa8b18a182e
Nina Kristel Cheng <nkmcheng@live.com>
2017-06-19 15:28:09 +0800

    add hits

---
+++ app/Models/Hit.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Hit extends Model
+{
+    use SoftDeletes;
+
+    protected $guarded = ['id'];
+
+    public function location()
+    {
+        return $this->belongsTo(ProjectLocation::class);
+    }
+
+    public function scopeLastUpdated()
+    {
+        return $this->orderBy('created_at', 'DESC')
+            ->first();
+    }
+}
---
+++ database/migrations/2017_03_20_202852_create_hits_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hits', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('project_location_id')->unsigned();
+            $table->timestamp('hit_timestamp');
+            $table->string('name')->nullable();
+            $table->string('email')->nullable();
+            $table->string('contact_number')->nullable();
+            $table->string('school_name')->nullable();
+            $table->string('designation')->nullable();
+            $table->string('address')->nullable();
+            $table->string('other_details')->nullable();
+            $table->string('image')->nullable();
+            $table->json('location')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('project_location_id')
+                ->references('id')
+                ->on('project_locations')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hits');
+    }
+}
