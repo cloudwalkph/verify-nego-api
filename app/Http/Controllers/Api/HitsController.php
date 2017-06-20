c625bac7becfe92d6a47d0057dc3866ce2fa8d13
Nina Kristel Cheng <nkmcheng@live.com>
2017-06-20 14:19:34 +0800

    validations

--- app/Http/Controllers/Api/HitsController.php
+++ app/Http/Controllers/Api/HitsController.php
@@ -2,13 +2,13 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CreateHitRequest;
 use App\Models\Hit;
 use Carbon\Carbon;
-use Illuminate\Http\Request;
 
 class HitsController extends Controller {
 
-    public function store(Request $request, $projectId)
+    public function store(CreateHitRequest $request, $projectId)
     {
         $input = $request->all();
         $filename = "";
@@ -21,15 +21,15 @@ class HitsController extends Controller {
             'user_id'               => $request->user()->id,
             'project_id'            => $projectId,
             'hit_timestamp'         => isset($input['hit_timestamp']) ? $input['hit_timestamp'] : Carbon::now()->toDateTimeString(),
-            'name'                  => isset($input['name']) ? $input['name'] : '',
-            'email'                 => isset($input['email']) ? $input['email'] : '',
-            'contact_number'        => isset($input['contact_number']) ? $input['contact_number'] : '',
-            'school_name'           => isset($input['school_name']) ? $input['school_name'] : '',
-            'designation'           => isset($input['designation']) ? $input['designation'] : '',
-            'address'               => isset($input['address']) ? $input['address'] : '',
+            'name'                  => $input['name'],
+            'email'                 => $input['email'],
+            'contact_number'        => $input['contact_number'],
+            'school_name'           => $input['school_name'],
+            'designation'           => $input['designation'],
+            'address'               => $input['address'],
             'other_details'         => isset($input['other_details']) ? $input['other_details'] : '',
             'image'                 => $filename,
-            'location'              => isset($input['location']) ? $input['location'] : ''
+            'location'              => $input['location']
         ];
 
         $newHit = Hit::create($hit);
---
+++ app/Http/Requests/CreateHitRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateHitRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required',
+            'contact_number' => 'required',
+            'address' => 'required',
+            'school_name' => 'required',
+            'designation' => 'required',
+            'location' => 'required',
+            'email' => 'required',
+        ];
+    }
+}
