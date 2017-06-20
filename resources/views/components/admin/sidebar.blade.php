c48893f698f424fa1ed7e70656dd71c2ec5617b3
Nina Kristel Cheng <nkmcheng@live.com>
2017-06-20 15:09:41 +0800

    update

--- resources/views/components/admin/sidebar.blade.php
+++ resources/views/components/admin/sidebar.blade.php
@@ -10,13 +10,13 @@
                 <a href="/home"><i class="fa fa-tachometer"></i><span> Dashboard</span></a>
             </li>
             <li>
-                <a href="/admin/users"><i class="fa fa-users"></i><span> Users</span></a>
+                <a href="/admin/tracking"><i class="fa fa-map-marker"></i><span> Tracking</span></a>
             </li>
             <li>
                 <a href="/admin/projects"><i class="fa fa-calendar-check-o"></i><span> Projects</span></a>
             </li>
             <li>
-                <a href="/admin/tracking"><i class="fa fa-map-marker"></i><span> Tracking</span></a>
+                <a href="/admin/users"><i class="fa fa-users"></i><span> Users</span></a>
             </li>
 
         </ul>
